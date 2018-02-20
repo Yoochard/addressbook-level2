2b3da247d164d7c547656e9ddc1b47c517593d25
Yang Yuqing <yangyuqinghhh@gmail.com>
2018-02-21 02:32:43 +0800

    set permission to private

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -6,7 +6,7 @@ package seedu.addressbook.data.person;
  */
 public class Contact {
 
-    public final String value;
+    private final String value;
     private boolean isPrivate;
 
     public Contact(String value, Boolean isPrivate) {
@@ -26,6 +26,8 @@ public class Contact {
     public boolean isPrivate() {
         return isPrivate;
     }
+
+    public String getValue () { return value; }
 
     public boolean equals(Object other) {
         return other == this // short circuit if same object
